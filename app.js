0ef2be3935dc4804507f60e3f7927cc6973e869c
Mike Coram <mikec@intec.local>
2017-06-14 16:01:12 +0100

    Views

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 var express = require('express');
+var handlebars = require('express-handlebars');
 
 var app = express();
+
+app.engine('handlebars', handlebars({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+var server = app.listen(8000, () => {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Listening as http://%s:%s', host, port);
+});
