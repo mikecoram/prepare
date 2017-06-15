4013bb0acce600219349bcf09f37d99e0d23c373
Mike Coram <mikec@intec.local>
2017-06-15 15:00:22 +0100

    Enviroment variables and error handling

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require('express');
 var handlebars = require('express-handlebars');
 
@@ -9,6 +11,21 @@ app.engine('handlebars', handlebars({defaultLayout:'main'}));
 app.set('view engine', 'handlebars');
 
 routes(app);
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        status: err.status,
+        message: err.message,
+        error: app.get('env') === 'development' ? err : ''
+    });
+});
 
 var server = app.listen(8000, function () {
     var host = server.address().address;
