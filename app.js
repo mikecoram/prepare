b47e42536cd35bf2be1645a3019130e915462cf4
Mike Coram <mikec@intec.local>
2017-06-15 14:28:14 +0100

    todo

--- app.js
+++ app.js
@@ -14,5 +14,5 @@ var server = app.listen(8000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Listening as http://%s:%s', host, port);
+    console.log('Listening as http://%s:%s', 'localhost', port);
 });
