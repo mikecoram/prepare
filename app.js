dca503b4f645c7cff1c3982d2e847acf9dcb768d
mikecoram <mikecoram96@gmail.com>
2017-06-17 14:55:45 +0100

    Begginings of auth

--- app.js
+++ app.js
@@ -14,6 +14,24 @@ app.set('view engine', 'handlebars');
 
 routes(app);
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
