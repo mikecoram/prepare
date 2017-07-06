c7f9538fcba230a20d5e8695925f9dee3adfd274
mikecoram <mikecoram96@gmail.com>
2017-07-06 16:07:10 +0100

    sign up form

--- routes.js
+++ routes.js
@@ -3,6 +3,10 @@ var main = require('./handlers/main');
 module.exports = function (app, passport) {
     app.get('/', main.home);
 
+    app.get('/signup', function (req, res) {
+        res.render('signup');
+    });
+
     app.post('/signup', passport.authenticate('local-signup', {
         successRedirect: '/dashboard',
         failureRedirect: '/',
