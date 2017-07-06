2013fa95c81bbe937b48d7f4722c43d10740418c
mikecoram <mikecoram96@gmail.com>
2017-07-06 16:24:30 +0100

    Sereialize + deserializse!

--- config/passport.js
+++ config/passport.js
@@ -42,4 +42,18 @@ module.exports = function (passport, user) {
             }
         });
     }));
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id).then(function(user) {
+            if (user) {
+                done(null, user.get());
+            } else {
+                done(user.errors, null);
+            }
+        });
+    });
 }
