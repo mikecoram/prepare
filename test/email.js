cab9cee3d5b27620fe1e7d9d047e724b9ca9b92b
mikecoram <mikecoram96@gmail.com>
2017-07-11 12:32:45 +0100

    Fix email test

--- test/email.js
+++ test/email.js
@@ -1,7 +1,7 @@
 var emailHelper = require('../lib/email');
 
 describe('email', function () {
-    it ('sends', function ()  {
+    it ('sends', function (done)  {
         emailHelper.sendMail({
             to: 'mkcoram@gmail.com',
             subject: 'Test email',
@@ -9,6 +9,8 @@ describe('email', function () {
             context: {
                 dateTime: new Date()
             }
+        }, function (err) {
+            done(err);
         });
     });
 });
