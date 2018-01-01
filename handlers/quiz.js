4f7dc163789e84ab641282ac6088c86470c8ea61
mikecoram <mikecoram96@gmail.com>
2018-01-01 20:16:57 +0000

    Move quiz marking code into a new module.

--- handlers/quiz.js
+++ handlers/quiz.js
@@ -1,4 +1,5 @@
 const { Quiz, Section, Question, Example, ExampleTemplate } = require('../models/');
+const QuizMarker = require('../lib/quiz-marker');
 
 exports.quiz = function(req, res) {
     getUserQuiz(req.user).then((quiz) => {
@@ -21,7 +22,7 @@ exports.intro = function(req, res) {
 
 exports.finish = function(req, res) {
     // mark quiz
-    markQuiz(req.user).then((result) => {
+    QuizMarker.markQuiz(req.user).then((result) => {
         res.redirect('/quiz/results');
     });
 }
@@ -44,80 +45,6 @@ exports.results = function(req, res) {
             completed: false
         });
     });
-}
-
-function markQuiz(user) {
-    return new Promise((resolve, reject) => {
-        Quiz.find({
-            where: {
-                userId: user.id,
-                finishedOn: null
-            },
-            include: [{
-                model: Section,
-                as: 'sections',
-                include: [{
-                    model: Question,
-                    as: 'questions'
-                }]
-            }]
-        }).then((quiz) => {
-
-            let seq = [];
-            let results = [];
-
-            // mark questions
-            quiz.sections.forEach((s) => {
-                s.questions.forEach((q) => {
-                    let r = markQuestion(q);
-                    results.push(r);
-
-                    seq.push(Question.update({
-                        correct: r
-                    }, {
-                        where: {
-                            id: q.id
-                        }
-                    }));
-                });
-            });
-
-            // grade quiz and set finished
-            seq.push(Quiz.update({
-                finishedOn: new Date(),
-                result: gradeQuiz(results, quiz.difficulty)
-            }, {
-                where: {
-                    userId: user.id,
-                    finishedOn: null
-                }
-            }));
-
-            Promise.all(seq).then((results) => {
-                resolve();
-            }, (err) => {
-                reject(err);
-            });
-        });
-    });
-}
-
-function markQuestion(question, difficulty) {
-    // Some questions are meant to be left blank
-    if (question.expectedOutput == '' && question.userOutput == null) {
-        return true;
-    } 
-
-    if (question.userOutput) {
-        return question.userOutput == question.expectedOutput;
-    }
-    else {
-        return false;
-    }
-}
-
-function gradeQuiz(results) {
-    return (results.filter((r) => {return r == true;}).length / results.length) * 100;
 }
 
 exports.uploadAnswers = function(req, res) {
