3f15035a6bf33a631a68f8445d27534458dcbbde
mikecoram <mikecoram96@gmail.com>
2018-01-02 17:42:17 +0000

    Port

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ app.use(function (err, req, res, next) {
 });
 
 // Listen
-var server = app.listen(8000, function () {
+var server = app.listen(process.env.PORT || 8000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
