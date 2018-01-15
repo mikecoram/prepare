e6e9a58615ffc5bd04ab756f8430795f7ad64f08
mikecoram <mikecoram96@gmail.com>
2018-01-15 11:52:49 +0000

    Quiz results graph

--- public/js/user-results.js
+++ public/js/user-results.js
@@ -1,22 +1,41 @@
-let lineChart = new Chart('line-graph-container', {
-    type: 'line',
-    xAxisID: 'Quiz #',
-    data: {
-        labels: graphData.labels,
-        datasets: [
-            {
-                backgroundColor	: '#3e95cd',
-                label: 'Result',
-                lineTension	: 0,
-                data: graphData.marks,
-                borderColor: "#3e95cd",
-                fill: false,
+if (graphData.marks.length > 0) {
+    let lineChart = new Chart('line-graph-container', {
+        type: 'line',
+        data: {
+            labels: graphData.labels,
+            datasets: [
+                {
+                    backgroundColor	: '#3e95cd',
+                    label: 'Result',
+                    lineTension	: 0,
+                    data: graphData.marks,
+                    borderColor: "#3e95cd",
+                    fill: false,
+                }
+            ]
+        },
+        options: {
+            animation: {
+                duration: 0
+            },
+            scales: {
+                yAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Mark'
+                    },
+                    ticks: {
+                        beginAtZero: true,
+                        max: 100
+                    }
+                }],
+                xAxes: [{
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Quiz'
+                    }
+                }]
             }
-        ]
-    },
-    options: {
-        animation: {
-            duration: 0
         }
-    }
-});
+    });
+}
