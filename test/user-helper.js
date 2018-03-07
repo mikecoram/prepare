291c10e7894130d6c8e8165fdb32b0e00753e2cd
mikecoram <mikecoram96@gmail.com>
2018-03-07 14:09:58 +0000

    Consistent naming

--- test/user-helper.js
+++ test/user-helper.js
@@ -1,7 +1,7 @@
 const UserHelper = require('../lib/user-helper');
 const TestUser = require('./lib/user');
 
-describe('user-helper', () => {
+describe('user helper', () => {
     it('retrieves the correct user', done => {
         let test = async function() {
             let createdUser = await TestUser.createTestUser();
